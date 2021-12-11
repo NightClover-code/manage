f24efb7df698906475f4832065fae933e77b9945
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-12-11 22:19:34 +0100

    experimenting with tailwind

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -3,7 +3,7 @@ import NavBar from './NavBar';
 const Header = () => {
   return (
     <header>
-      <div className="container">
+      <div className="container w-10/12 mx-auto overflow-hidden">
         <NavBar />
       </div>
     </header>
