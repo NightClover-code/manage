2e7a00c8bbc276282c701308e8c35c2a022207b7
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-12-16 15:06:42 +0100

    adding tailwind custom colors, font size and family

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,16 +4,21 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {
-      colors: {
-        orange: 'rgb(249, 119, 54)',
-        blue: 'rgb(33, 63, 112)',
-        offBlue: 'rgba(36, 45, 82, 0.8)',
-        offGray: 'rgba(36, 45, 82, 0.6)',
-        pink: 'rgb(255, 249, 246)',
-        darkGray: 'rgb(30, 30, 38)',
-        offWhite: 'rgba(255, 255, 255, 0.6)',
-      },
+    colors: {
+      orange: 'rgb(249, 119, 54)',
+      blue: 'rgb(33, 63, 112)',
+      offBlue: 'rgba(36, 45, 82, 0.8)',
+      offGray: 'rgba(36, 45, 82, 0.6)',
+      pink: 'rgb(255, 249, 246)',
+      darkGray: 'rgb(30, 30, 38)',
+      offWhite: 'rgba(255, 255, 255, 0.6)',
+      darkOverlay: 'rgba(37, 43, 70, 0.95)',
+    },
+    fontSize: {
+      sm: '18px',
+    },
+    fontFamily: {
+      mulish: ['Mulish', 'sans-serif'],
     },
   },
   plugins: [],
