712ab202a4ba58366e49a7fba64e93c74e9c543c
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-12-17 02:47:05 +0100

    added icons to hero section

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -6,6 +6,16 @@ const Hero = () => {
   return (
     <section className="hero__section">
       <div className="custom__container">
+        <div className="w-[150px] h-[150px] absolute left-24 top-[40rem]">
+          <Image src="/images/icon-bell.svg" layout="fill" alt="bell icon" />
+        </div>
+        <div className="w-[150px] h-[150px] absolute right-32 top-[26rem]">
+          <Image
+            src="/images/icon-calendar.svg"
+            layout="fill"
+            alt="calendar icon"
+          />
+        </div>
         <h1 className="mx-auto mt-28">
           Manage projects properly. <span className="text-orange">Plan</span>{' '}
           with purpose.
@@ -16,7 +26,7 @@ const Hero = () => {
         </p>
         <div className="flex__center mt-10">
           <Button className="mr-5">Download</Button>
-          <Button className="!text-blue !bg-white">Register</Button>
+          <Button className="!text-blue !bg-white !px-[27px]">Register</Button>
         </div>
         <div className="relative w-[1066px] h-[695px] mt-10 mx-auto">
           <Image src="/images/mockup.svg" layout="fill" alt="mockup" />
