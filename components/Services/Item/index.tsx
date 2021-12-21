a49a48e340ace2daeeed28ce96103e52564477a7
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-12-21 14:44:54 +0100

    completed services initial styling

--- components/Services/Item/index.tsx
+++ components/Services/Item/index.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import Button from '../../Button';
 
 const Service = () => {
@@ -7,9 +8,33 @@ const Service = () => {
       <h1 className="mt-4">$21</h1>
       <h3 className="mt-3 text-sm font-semibold">Per Month</h3>
       <ul className="text-left text-offBlue text-xxs mt-6">
-        <li className="py-2">Unlimited Draft</li>
-        <li className="py-2">Limit 12 schedule</li>
-        <li className="py-2">Basic Settings</li>
+        <li className="py-2 flex__center">
+          <Image
+            src="/images/checkmark.svg"
+            width={15}
+            height={15}
+            alt="checkmark"
+          />
+          <span className="mx-3">Unlimited Draft</span>
+        </li>
+        <li className="py-2 flex__center">
+          <Image
+            src="/images/checkmark.svg"
+            width={15}
+            height={15}
+            alt="checkmark"
+          />
+          <span className="mx-3">Unlimited Draft</span>
+        </li>
+        <li className="py-2 flex__center">
+          <Image
+            src="/images/checkmark.svg"
+            width={15}
+            height={15}
+            alt="checkmark"
+          />
+          <span className="mx-3">Unlimited Draft</span>
+        </li>
       </ul>
       <Button className="text-orange font-bold w-[85%] border-2 rounded-full border-orange py-[10px] mt-auto mb-8">
         Get Started
