e807ceab0b0efc4d49329f5a58feb5f8186b3b22
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-12-21 10:39:50 +0100

    styling toggle button

--- components/Services/index.tsx
+++ components/Services/index.tsx
@@ -9,13 +9,15 @@ const Services = () => {
           We provide several plans for you to choose from, pick the most
           suitable for your business.
         </p>
-        <div className="flex__center mt-7">
-          <div className="flex">
+        <div className="flex__center mt-7 ml-20">
+          <div className="flex items-center">
             <span className="text-xs">Bill monthly</span>
-            <div className="toggle"></div>
+            <div className="flex items-center w-[36px] h-[18px] bg-orange mx-6 rounded-[10px] cursor-pointer">
+              <div className="block w-[13px] h-[13px] bg-white rounded-[10px] ml-1"></div>
+            </div>
             <span className="text-xs">Bill annualy</span>
           </div>
-          <span className="text-xxs text-red">17% Off</span>
+          <span className="text-xxs text-red ml-7">17% Off</span>
         </div>
       </div>
     </section>
