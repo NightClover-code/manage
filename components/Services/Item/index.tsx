0739e57c9e78f4651254c3361283cfa57bd7ff76
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-12-21 11:48:27 +0100

    more service items styling

--- components/Services/Item/index.tsx
+++ components/Services/Item/index.tsx
@@ -2,11 +2,18 @@ import Button from '../../Button';
 
 const Service = () => {
   return (
-    <div className="bg-lightOrange rounded-[20px] shadow-service pt-10">
+    <div className="flex items-center flex-col bg-lightOrange rounded-[20px] shadow-service pt-10">
       <span className="text-[20px] font-semibold">Standart</span>
       <h1 className="mt-4">$21</h1>
       <h3 className="mt-3 text-sm font-semibold">Per Month</h3>
-      <Button className="bg-transparent">Get Started</Button>
+      <ul className="text-left text-offBlue text-xxs mt-6">
+        <li className="py-2">Unlimited Draft</li>
+        <li className="py-2">Limit 12 schedule</li>
+        <li className="py-2">Basic Settings</li>
+      </ul>
+      <Button className="text-orange font-bold w-[85%] border-2 rounded-full border-orange py-[10px] mt-auto mb-8">
+        Get Started
+      </Button>
     </div>
   );
 };
