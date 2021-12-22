220ebc5e40e963545a1148ebfe0a9f70b9afac58
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-12-22 16:17:26 +0100

    cleanup

--- components/Achievements/Item/index.tsx
+++ components/Achievements/Item/index.tsx
@@ -7,12 +7,14 @@ const Achievement: React.FC<AchievementInterface> = ({
   borderLeft,
 }) => {
   return (
-    <div className={borderLeft ? 'border-l-2 border-gray' : ''}>
+    <div
+      className={borderLeft ? 'border-l-[1px] border-solidGray py-2' : 'py-2'}
+    >
       <h2 className="text-orange">
         {result}
         {resultType}
       </h2>
-      <h3 className="text-offGray mt-5">{title}</h3>
+      <h3 className="text-offGray mt-3 font-medium">{title}</h3>
     </div>
   );
 };
